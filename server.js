031152c5a5f52030d2b34c2e0bda614ca5d09aa1
Kyle Gracey <kyle@kylegracey.com>
2017-05-02 16:42:05 -0500

    Finished with tut

--- server.js
+++ server.js
@@ -50,3 +50,12 @@ app.put('/quotes', (req, res) => {
       res.send(result)
     })
 })
+
+app.delete('/quotes', (req, res) => {
+  // Handle delete event
+  db.collection('quotes').findOneAndDelete({ name: req.body.name },
+    (err, result) => {
+        if (err) return res.send(500, err)
+        res.send('A darth vadar quote got deleted')
+    })
+})
