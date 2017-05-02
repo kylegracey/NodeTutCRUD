ce6c20f6c9cfc39d140797350ee7d6adcf46bbae
Kyle Gracey <kyle@kylegracey.com>
2017-05-02 15:37:28 -0500

    Connected to db

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
 const app = express();
+
+MongoClient.connect('mongodb://nodetut:cruduser@ds129281.mlab.com:29281/nodetutcrud', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function(){
-  console.log('listening on 3000');
-});
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-});
+})
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body);
-});
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
